984668e968a45a9115b6e017ef10fd4e530afcbd
SnezhG <stim.nuzhno@mail.ru>
2025-01-05 21:06:54 +0300

    Рефакторинг навигации

--- App.js
+++ App.js
@@ -1,6 +1,103 @@
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import React, {useContext} from 'react';
+import AppNavigator from "./src/navigation/AppNavigator";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {AuthContext, AuthProvider} from "./src/auth/AuthContext";
+import LoadingScreen from "./src/screens/auth/LoadingScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import RegisterScreen from "./src/screens/auth/RegisterScreen";
+import LoginScreen from "./src/screens/auth/LoginScreen";
+import MainAppStack from "./src/navigation/MainAppStack";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+const Stack = createNativeStackNavigator();
+
+const AuthStack = () => (
+    <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+);
+
+const AppStack = () => (
+    <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+);
+
+const AppNavigation = () => {
+    const { isAuthenticated, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <LoadingScreen />;
+    }
+
+    return (
+        <NavigationContainer>
+            {isAuthenticated ? (
+                <Stack.Navigator screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="MainApp" component={MainAppStack} />
+                </Stack.Navigator>
+            ) : (
+                <AuthStack />
+            )}
+
+        </NavigationContainer>
+    );
+};
 
 export default function App() {
-    return <AppNavigator />;
+    const [expoPushToken, setExpoPushToken] = useState('');
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        const subscription = Notifications.addNotificationReceivedListener(notification => {
+            console.log('Notification received!', notification);
+        });
+
+        return () => subscription.remove();
+    }, []);
+
+    return (
+        <AuthProvider>
+            <AppNavigator />
+        </AuthProvider>
+    );
 }
+
+
+
+async function registerForPushNotificationsAsync() {
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+        return token;
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+}
+
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,96 +1,43 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {AuthContext, AuthProvider} from "../auth/AuthContext";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
+import LoadingScreen from "../screens/auth/LoadingScreen";
+import MainAppStack from "./MainAppStack";
 
-import HomeScreen from '../screens/HomeScreen';
-import PetProfileScreen from "../screens/pet/PetProfileScreen";
-
-import PetEventCreateScreen from "../screens/pet-event/PetEventCreateScreen";
-import PetEventsListScreen from "../screens/pet-event/PetEventsListScreen";
-import PetEventEditFormScreen from "../screens/pet-event/PetEventEditFormScreen";
-import PetEventsByDayScreen from "../screens/pet-event/PetEventsByDayScreen";
-import PetEventViewScreen from "../screens/pet-event/PetEventViewScreen";
-
-import PetClinicMapScreen from "../screens/map/PetClinicMapScreen";
-import PetCreateScreen from "../screens/pet/PetCreateScreen";
-import PetEditFormScreen from "../screens/pet/PetEditFormScreen";
-
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const TabNavigator = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-
-                    if (route.name === 'Home') {
-                        iconName = focused ? 'paw' : 'paw-outline';
-                    } else if (route.name === 'Event') {
-                        iconName = focused ? 'calendar' : 'calendar-outline';
-                    } else if (route.name === 'PetMap') {
-                        iconName = focused ? 'map' : 'map-outline';
-                    }
-
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: 'blue',
-                tabBarInactiveTintColor: 'gray',
-                headerStyle: {
-                    backgroundColor: '#4CAF50',
-                    marginBottom: 15,
-                },
-                headerTitleStyle: {
-                    color: '#fff',
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                },
-                headerTitle: 'Pet-App',
-            })}
-        >
-
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Event" component={PetEventsListScreen} />
-            <Tab.Screen name="PetMap" component={PetClinicMapScreen} />
-        </Tab.Navigator>
-    );
-};
+const AuthStack = () => (
+    <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+);
 
 const AppNavigator = () => {
+    const { isAuthenticated, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <LoadingScreen />;
+    }
+
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: '#4CAF50',
-                    },
-                    headerTitleStyle: {
-                        color: '#fff',
-                        fontSize: 20,
-                        fontWeight: 'bold',
-                    },
-                    headerTitle: 'Pet-App',
-                }}
-            >
-                <Stack.Screen
-                    name="MainTabs"
-                    component={TabNavigator}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen name="PetProfile" component={PetProfileScreen} />
-                <Stack.Screen name="PetCreate" component={PetCreateScreen} />
-                <Stack.Screen name="PetEdit" component={PetEditFormScreen} />
-
-                <Stack.Screen name="EventCreate" component={PetEventCreateScreen} />
-                <Stack.Screen name="EventEdit" component={PetEventEditFormScreen} />
-                <Stack.Screen name="EventsByDay" component={PetEventsByDayScreen} />
-                <Stack.Screen name="EventView" component={PetEventViewScreen} />
-            </Stack.Navigator>
+            {isAuthenticated ? (
+                <MainAppStack />
+            ) : (
+                <AuthStack />
+            )}
         </NavigationContainer>
     );
 };
 
-export default AppNavigator;
+export default function App() {
+    return (
+        <AuthProvider>
+            <AppNavigator />
+        </AuthProvider>
+    );
+}
